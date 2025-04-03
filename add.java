39973d23b3d110bab9e50492bac33345b37bd451
PriyankaD24 <priyankadonaj11@gmail.com>
2025-04-03 15:16:22 +0530

    Update add.java

--- add.java
+++ add.java
@@ -14,9 +14,11 @@ public class AdditionProgram {
 
         // Calculate sum
         double sum = num1 + num2;
+        double product = num1 * num2;
 
         // Display result
         System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+        System.out.println("The multiplication of " + num1 + " and " + num2 + " is: "+ product);
 
         // Close the scanner
         scanner.close();
